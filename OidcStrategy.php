1e9c5ec6cea4ee05ecd5e7ada95402edc228d3ec
Ivan Cvitkovic <ivanc@uw.edu>
2024-02-02 13:47:30 -0800

    Initial refactor to OIDC

--- OidcStrategy.php
+++ OidcStrategy.php
@@ -1,151 +1,150 @@
 <?php
 /**
- * Google strategy for Opauth
- * based on https://developers.google.com/accounts/docs/OAuth2
- * 
+ * OpenID Connect strategy for Opauth
+ *
  * More information on Opauth: http://opauth.org
- * 
- * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
- * @link         http://opauth.org
- * @package      Opauth.GoogleStrategy
+ *
+ * @package      Opauth.OidcStrategy
  * @license      MIT License
  */
 
-/**
- * Google strategy for Opauth
- * based on https://developers.google.com/accounts/docs/OAuth2
- * 
- * @package			Opauth.Google
- */
-class GoogleStrategy extends OpauthStrategy{
-	
-	/**
-	 * Compulsory config keys, listed as unassociative arrays
-	 */
-	public $expects = array('client_id', 'client_secret');
-	
-	/**
-	 * Optional config keys, without predefining any default values.
-	 */
-	public $optionals = array('redirect_uri', 'scope', 'state', 'access_type', 'approval_prompt');
-	
-	/**
-	 * Optional config keys with respective default values, listed as associative arrays
-	 * eg. array('scope' => 'email');
-	 */
-	public $defaults = array(
-		'redirect_uri' => '{complete_url_to_strategy}oauth2callback',
-		'scope' => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
-	);
-	
-	/**
-	 * Auth request
-	 */
-	public function request(){
-		$url = 'https://accounts.google.com/o/oauth2/auth';
-		$params = array(
-			'client_id' => $this->strategy['client_id'],
-			'redirect_uri' => $this->strategy['redirect_uri'],
-			'response_type' => 'code',
-			'scope' => $this->strategy['scope']
-		);
+class OidcStrategy extends OpauthStrategy{
 
-		foreach ($this->optionals as $key){
-			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
-		}
-		
-		$this->clientGet($url, $params);
-	}
-	
-	/**
-	 * Internal callback, after OAuth
-	 */
-	public function oauth2callback(){
-		if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
-			$code = $_GET['code'];
-			$url = 'https://accounts.google.com/o/oauth2/token';
-			$params = array(
-				'code' => $code,
-				'client_id' => $this->strategy['client_id'],
-				'client_secret' => $this->strategy['client_secret'],
-				'redirect_uri' => $this->strategy['redirect_uri'],
-				'grant_type' => 'authorization_code'
-			);
-			$response = $this->serverPost($url, $params, null, $headers);
-			
-			$results = json_decode($response);
-			
-			if (!empty($results) && !empty($results->access_token)){
-				$userinfo = $this->userinfo($results->access_token);
-				
-				$this->auth = array(
-					'uid' => $userinfo['id'],
-					'info' => array(),
-					'credentials' => array(
-						'token' => $results->access_token,
-						'expires' => date('c', time() + $results->expires_in)
-					),
-					'raw' => $userinfo
-				);
+    /**
+     * Compulsory config keys, listed as unassociative arrays
+     */
+    public $expects = array('client_id', 'client_secret');
 
-				if (!empty($results->refresh_token))
-				{
-					$this->auth['credentials']['refresh_token'] = $results->refresh_token;
-				}
-				
-				$this->mapProfile($userinfo, 'name', 'info.name');
-				$this->mapProfile($userinfo, 'email', 'info.email');
-				$this->mapProfile($userinfo, 'given_name', 'info.first_name');
-				$this->mapProfile($userinfo, 'family_name', 'info.last_name');
-				$this->mapProfile($userinfo, 'picture', 'info.image');
-				
-				$this->callback();
-			}
-			else{
-				$error = array(
-					'code' => 'access_token_error',
-					'message' => 'Failed when attempting to obtain access token',
-					'raw' => array(
-						'response' => $response,
-						'headers' => $headers
-					)
-				);
+    /**
+     * Optional config keys, without predefining any default values.
+     */
+    public $optionals = array(
+        'redirect_uri',
+        'scope',
+        'authorization_endpoint',
+        'token_endpoint',
+        'userinfo_endpoint',
+    );
+    /**
+     * Optional config keys with respective default values, listed as associative arrays
+     * eg. array('scope' => 'email');
+     */
+    public $defaults = array(
+        'redirect_uri' => '{complete_url_to_strategy}oauth2callback',
+        'scope' => 'profile email openid'
+    );
 
-				$this->errorCallback($error);
-			}
-		}
-		else{
-			$error = array(
-				'code' => 'oauth2callback_error',
-				'raw' => $_GET
-			);
-			
-			$this->errorCallback($error);
-		}
-	}
-	
-	/**
-	 * Queries Google API for user info
-	 *
-	 * @param string $access_token 
-	 * @return array Parsed JSON results
-	 */
-	private function userinfo($access_token){
-		$userinfo = $this->serverGet('https://www.googleapis.com/oauth2/v1/userinfo', array('access_token' => $access_token), null, $headers);
-		if (!empty($userinfo)){
-			return $this->recursiveGetObjectVars(json_decode($userinfo));
-		}
-		else{
-			$error = array(
-				'code' => 'userinfo_error',
-				'message' => 'Failed when attempting to query for user information',
-				'raw' => array(
-					'response' => $userinfo,
-					'headers' => $headers
-				)
-			);
+    /**
+     * Auth request
+     */
+    public function request(){
+        $params = array(
+            'client_id' => $this->strategy['client_id'],
+            'redirect_uri' => $this->strategy['redirect_uri'],
+            'response_type' => 'code',
+            'scope' => $this->strategy['scope']
+        );
 
-			$this->errorCallback($error);
-		}
-	}
+        $this->clientGet($this->strategy['authorization_endpoint'], $params);
+    }
+
+    /**
+     * Internal callback, after OAuth
+     */
+    public function oauth2callback(){
+        if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
+            $code = $_GET['code'];
+            $params = array(
+                'code' => $code,
+                'client_id' => $this->strategy['client_id'],
+                'client_secret' => $this->strategy['client_secret'],
+                'redirect_uri' => $this->strategy['redirect_uri'],
+                'grant_type' => 'authorization_code'
+            );
+            $response = $this->serverPost(
+                $this->strategy['token_endpoint'],
+                $params,
+                null,
+                $response_headers
+            );
+
+            $results = json_decode($response);
+
+            if (!empty($results) && !empty($results->access_token)){
+                $userinfo = $this->userinfo($results->access_token);
+                $this->auth = array(
+                    'sub' => $userinfo['sub'],
+                    'info' => array(),
+                    'credentials' => array(
+                        'token' => $results->access_token,
+                        'expires' => date('c', time() + $results->expires_in)
+                    ),
+                    'raw' => $userinfo
+                );
+
+                if (!empty($results->refresh_token)){
+                    $this->auth['credentials']['refresh_token'] = $results->refresh_token;
+                }
+
+                // map OIDC user attributes to cPRO-specific names
+                $this->mapProfile($userinfo, 'name', 'name');
+                $this->mapProfile($userinfo, 'username', 'preferred_username');
+                $this->mapProfile($userinfo, 'given_name', 'given_name');
+                $this->mapProfile($userinfo, 'family_name', 'family_name');
+                $this->mapProfile($userinfo, 'email_verified', 'email_verified');
+                $this->callback();
+            }
+            else{
+                $error = array(
+                    'code' => 'access_token_error',
+                    'message' => 'Failed when attempting to obtain access token',
+                    'raw' => array(
+                        'response' => $response,
+                        'headers' => $response_headers
+                    )
+                );
+
+                $this->errorCallback($error);
+            }
+        }
+        else{
+            $error = array(
+                'code' => 'oauth2callback_error',
+                'raw' => $_GET
+            );
+
+            $this->errorCallback($error);
+        }
+    }
+
+    /**
+     * Queries auth API for user info
+     *
+     * @param string $access_token
+     * @return array Parsed JSON results
+     */
+    private function userinfo($access_token){
+        // TODO look from JWT, when available
+        $userinfo = $this->serverGet(
+            $this->strategy['userinfo_endpoint'],
+            array(),
+            array('http' => array('header' => "Authorization: Bearer ${access_token}")),
+            $response_headers
+        );
+        if (!empty($userinfo)){
+            return $this->recursiveGetObjectVars(json_decode($userinfo));
+        }
+        else{
+            $error = array(
+                'code' => 'userinfo_error',
+                'message' => 'Failed when attempting to query for user information',
+                'raw' => array(
+                    'response' => $userinfo,
+                    'headers' => $response_headers
+                )
+            );
+
+            $this->errorCallback($error);
+        }
+    }
 }
